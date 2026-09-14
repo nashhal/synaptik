a0ed8d0ddf30d01bf2a8b3a37813c226b72e6f71
nashhal <husseinalyfai@gmail.com>
2026-09-14 10:51:54 +0300

    Fix GitHub Pages base path for Synaptik

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
+  basePath: '/synaptik',
+  assetPrefix: '/synaptik/',
   images: { unoptimized: true }
 };
 module.exports = nextConfig;
